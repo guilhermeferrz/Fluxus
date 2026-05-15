901260250a03e9892bf4bc0f6ce9f2065974517c
Giovanna do Vale Santos <giovannadovalesantos@gmail.com>
2026-05-15 19:12:16 -0300

    Update main_api_fluxus.py

--- main_api_fluxus.py
+++ main_api_fluxus.py
@@ -37,11 +37,18 @@ except Exception as e:
     print(f"❌ Erro na conexão: {e}")
 
 # 3. Endpoint de Consulta
-@app.get("/sacado/{cnpj}", summary="Consulta de Sacado")
+app.get("/sacado/{cnpj:path}", summary="Consulta de Sacado")
 def obter_risco_sacado(cnpj: str):
-    # Remove pontos e traços do CNPJ se o usuário digitar formatado
+    # 1. Limpa qualquer sujeira que vier do front-end
     cnpj_limpo = "".join(filter(str.isdigit, cnpj))
-    chave = f"feature:sacado:{cnpj_limpo}"
+    
+    # 2. Garante que tem 14 números para não quebrar a formatação
+    if len(cnpj_limpo) != 14:
+        raise HTTPException(status_code=400, detail="Formato de CNPJ inválido")
+        
+    # 3. Recria a máscara perfeita igualzinha à que está no Redis
+    cnpj_banco = f"{cnpj_limpo[:2]}.{cnpj_limpo[2:5]}.{cnpj_limpo[5:8]}/{cnpj_limpo[8:12]}-{cnpj_limpo[12:]}"
+    chave = f"feature:sacado:{cnpj_banco}"
     
     dados = r.get(chave)
     if dados:
